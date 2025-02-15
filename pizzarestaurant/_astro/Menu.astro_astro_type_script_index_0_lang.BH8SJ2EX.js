ef05add6d19c313ec04c8cade51800ca551e272b
TAnas0 <TAnas0@users.noreply.github.com>
2025-02-15 10:15:52 +0000

    deploy: ae02e36a0d1fc7abd63c95a65b3a91c5a670abae

---
+++ pizzarestaurant/_astro/Menu.astro_astro_type_script_index_0_lang.BH8SJ2EX.js
@@ -0,0 +1,1 @@
+import{S as s}from"./swiper.BGazw6qc.js";document.addEventListener("astro:page-load",()=>{const e=document.querySelector(".swiper-plates");new s(e,{loop:!1,spaceBetween:32,slidesPerView:"auto"})});
