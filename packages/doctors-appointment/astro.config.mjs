cdd8e018837b2f5349a813ff5719b63d8b802d92
TAnas0 <anastiour@gmail.com>
2025-02-23 19:45:53 +0000

    env(dokto): use env var for base Astro config option

--- packages/doctors-appointment/astro.config.mjs
+++ packages/doctors-appointment/astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
+const base = process.env.BASE_URL || "/catalog/dokto";
+
 // https://astro.build/config
 export default defineConfig({
-  base: "/catalog/dokto",
+  base,
   outDir: "../../dist/dokto",
   integrations: [
     tailwind()
